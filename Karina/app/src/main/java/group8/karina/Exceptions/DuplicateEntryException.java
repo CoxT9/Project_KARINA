14ce4246e00fc4c23d7eaf410e88d6cc7c83e427
malcolm-mergulhao <mmerg_1995@hotmail.com>
2016-05-29 17:39:54 -0500

    removed bad files

--- Karina/app/src/main/java/group8/karina/Exceptions/DuplicateEntryException.java
+++
@@ -1,9 +0,0 @@
-package group8.karina.Exceptions;
-
-public class DuplicateEntryException extends Exception
-{
-    public DuplicateEntryException(String message)
-    {
-        super(message);
-    }
-}
