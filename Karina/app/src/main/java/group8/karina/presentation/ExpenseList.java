d523c2441baba1a9e0e513c8969d2314757b366e
Caden Marofke <cadenmarofke@cadens-mbp.gateway.mts.net>
2016-06-15 20:47:30 -0500

    removed unused import

--- Karina/app/src/main/java/group8/karina/presentation/ExpenseList.java
+++ Karina/app/src/main/java/group8/karina/presentation/ExpenseList.java
@@ -9,7 +9,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import group8.karina.R;
