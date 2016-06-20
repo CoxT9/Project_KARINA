4c0b97936ce48ea387b9644d4120077e577451fa
Sam Morrison <sam.morrison@invenia.ca>
2016-06-20 15:01:12 -0500

    prettified the totals report

--- Karina/app/src/main/java/group8/karina/presentation/TotalTransactionReportActivity.java
+++ Karina/app/src/main/java/group8/karina/presentation/TotalTransactionReportActivity.java
@@ -21,6 +21,7 @@ public class TotalTransactionReportActivity extends AppCompatActivity
 {
 	private TextView totalIncome;
 	private TextView totalExpense;
+	private TextView totalNet;
 	private AccessTransactions transactions;
 	private PieChart pieChart;
 
@@ -29,20 +30,37 @@ public class TotalTransactionReportActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_total_transaction_report);
-
 		transactions = new AccessTransactions();
+
+		formatTextLabels();
+		initializePieChart();
+
+	}
+
+	private void formatTextLabels()
+	{
+		double netMonies = transactions.totalNet();
 
 		totalIncome = (TextView) findViewById(R.id.incomeTotalValue);
 		totalExpense = (TextView) findViewById(R.id.expenseTotalValue);
+		totalNet = (TextView) findViewById(R.id.netTotalValue);
 		pieChart = (PieChart) findViewById(R.id.pieChart);
 
 		totalIncome.setText("$" + transactions.totalIncome());
 		totalExpense.setText("$" + transactions.totalExpenses());
 
-		initializePieChart();
-
+		//put into accounting format
+		if (netMonies<0)
+		{
+			totalNet.setTextColor(0xFFFF0000);
+			totalNet.setText("- $" + String.format("%.2f", netMonies*-1));
+		}
+		else
+		{
+			totalNet.setTextColor(totalIncome.getCurrentTextColor());
+			totalNet.setText("$" + String.format("%.2f", netMonies));
+		}
 	}
-
 	private void initializePieChart()
 	{
 		pieChart.setHoleRadius(40);
@@ -63,9 +81,10 @@ public class TotalTransactionReportActivity extends AppCompatActivity
 
 	private PieData createPieChartData()
 	{
+		double sumOfMonies = transactions.positiveSumTransactions();
 		ArrayList<Entry> entries = new ArrayList<Entry>();
-		entries.add(new Entry((float)transactions.totalIncome(),0));
-		entries.add(new Entry((float)transactions.totalExpenses(),1));
+		entries.add(new Entry((float)(transactions.totalIncome()*100/sumOfMonies),0));
+		entries.add(new Entry((float)(transactions.totalExpenses()*100/sumOfMonies),1));
 
 		ArrayList<String> titles = new ArrayList<String>();
 		titles.add("Income");
