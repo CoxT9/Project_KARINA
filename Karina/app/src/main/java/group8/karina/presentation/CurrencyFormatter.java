447188944d8298ae5c8909e360c9eda10cd427a9
Sam Morrison <sam.morrison@invenia.ca>
2016-07-04 17:57:29 -0500

    replace mistakenly deleted line and fix spelling

--- Karina/app/src/main/java/group8/karina/presentation/CurrencyFormatter.java
+++ Karina/app/src/main/java/group8/karina/presentation/CurrencyFormatter.java
@@ -1,6 +1,5 @@
 package group8.karina.presentation;
 
-import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.github.mikephil.charting.formatter.ValueFormatter;
@@ -10,8 +9,8 @@ import java.text.DecimalFormat;
 
 /**
  * This ValueFormatter is just for convenience and simply puts a "$" sign before
- * each value. (Recommeded for PieChart)(blatently stolen from mikephil charting)
- * It is modelled after the PerccentFormatter example by Philipp Jahoda
+ * each value. (Recommeded for PieChart)
+ * It is modelled after the PercentFormatter example by Philipp Jahoda
  */
 public class CurrencyFormatter implements ValueFormatter
 {
